0afaf5a0ac8876857b5e50c12d43c80d88fe92b7
Abhiraj Jaiswal <aaryanjyswal@gmail.com>
2022-03-20 08:43:40 +0545

    Login page of AIMS

---
+++ aims/lib/login_page.dart
@@ -0,0 +1,211 @@
+//import 'package:aims/home_page.dart';
+//import 'package:aims/registration_page.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  // firebase
+  final _auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    //Email Field
+    final emailField = TextFormField(
+      autofocus: false,
+      controller: emailController,
+      keyboardType: TextInputType.emailAddress,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'Please Enter Your username';
+        }
+        if (!RegExp(r'^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+')
+            .hasMatch(value)) {
+          return 'Please Enter a valid email';
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        prefixIcon: const Icon(Icons.email),
+        hintText: 'Email',
+        contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
+      ),
+      onSaved: (value) {
+        // This optional block of code can be used to run
+        // code when the user saves the form.
+        emailController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+    );
+
+    //Password Field
+    final passwordField = TextFormField(
+      autofocus: false,
+      controller: passwordController,
+      obscureText: true,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'Please Enter Your password';
+        }
+        RegExp regExp = new RegExp(r'^.{6,}$');
+        if (value.isEmpty) {
+          return 'Please Enter Your password';
+        }
+        if (!regExp.hasMatch(value)) {
+          return 'Password must be at least 6 characters';
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        prefixIcon: const Icon(Icons.lock),
+        hintText: 'Password',
+        contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
+      ),
+      onSaved: (value) {
+        // This optional block of code can be used to run
+        // code when the user saves the form.
+        passwordController.text = value!;
+      },
+      textInputAction: TextInputAction.done,
+    );
+
+    //Login Button
+    final loginButton = Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Colors.lightGreenAccent,
+      child: MaterialButton(
+        padding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        minWidth: MediaQuery.of(context).size.width,
+        onPressed: () {
+          signIn(emailController.text, passwordController.text);
+        },
+        child: const Text(
+          'Login',
+          style: TextStyle(color: Colors.black, fontSize: 15.0),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(50.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        SizedBox(
+                          height: 250.0,
+                          child: Image.asset(
+                            "assets/black_logo.png",
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        emailField,
+                        const SizedBox(
+                          height: 25,
+                        ),
+                        passwordField,
+                        const SizedBox(
+                          height: 35,
+                        ),
+                        loginButton,
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            const Text(
+                              'Don\'t have an account?',
+                              style: TextStyle(fontSize: 15),
+                            ),
+                            const SizedBox(
+                              width: 5,
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                // Navigator.push(
+                                //   context,
+                                //   MaterialPageRoute(
+                                //     builder: (context) =>
+                                //        const RegistrationPage(),
+                                //   ),
+                                // );
+                              },
+                              child: const Text(
+                                'Sign up',
+                                style: TextStyle(
+                                    fontSize: 15, color: Colors.green),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ]),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // login function
+  void signIn(String email, String password) async {
+    // validate form
+    if (_formKey.currentState!.validate()) {
+      await _auth
+          .signInWithEmailAndPassword(email: email, password: password)
+          .then((user) {
+        Fluttertoast.showToast(
+            msg: "Login Successful",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.green,
+            textColor: Colors.white,
+            fontSize: 16.0);
+        // Navigator.push(
+        //   context,
+        //   MaterialPageRoute(builder: (context) => const HomePage()),
+        // );
+      }).catchError((e) {
+        Fluttertoast.showToast(
+            msg: e.toString(),
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 16.0);
+      });
+    }
+  }
+}
